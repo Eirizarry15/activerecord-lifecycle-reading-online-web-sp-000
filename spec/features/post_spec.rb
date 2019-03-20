8bef3b2eb52256c1300588d94b5f4993932e3898
Edgar Irizarry <eirizarry15@yahoo.com>
2019-03-20 14:00:09 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,7 +5,7 @@ describe 'navigate' do
     @post = Post.create(title: "My Post", description: "My post desc")
   end
 
-  it 'shows the title on the show page in a h1 tag' do
+  it '' do
     visit "/posts/#{@post.id}"
     expect(page).to have_css("h1", text: "My Post")
   end
